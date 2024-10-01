9b4adf54b3a9f6f59ba1fa02e9fa5b8c5c84c503
abhirami102 <137210545+abhirami102@users.noreply.github.com>
2024-10-01 14:57:48 +0530

    Add files via upload

---
+++ studentdetails.py
@@ -0,0 +1,7 @@
+name=input("enter the student name:")
+roll_number=input("enter the roll number:")
+cgpa=float(input('enter the cgpa:'))
+percentage=cgpa*10
+print('Name of the student:', name)
+print('roll number:',roll_number)
+print('total percentage=,'  )
