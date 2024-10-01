9d6416e360dbda5f2f21885c7c9627f99c8eff2c
abhirami102 <137210545+abhirami102@users.noreply.github.com>
2024-10-01 15:08:43 +0530

    studentdetails.py
    first commit

--- studentdetails.py
+++ studentdetails.py
@@ -4,4 +4,4 @@ cgpa=float(input('enter the cgpa:'))
 percentage=cgpa*10
 print('Name of the student:', name)
 print('roll number:',roll_number)
-print('total percentage=,'  )
+print('total percentage=', pecentage)
