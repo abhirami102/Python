dfa08863237f9892c98f2b7f4d1ddf61e5716554
abhirami102 <137210545+abhirami102@users.noreply.github.com>
2024-10-01 15:20:00 +0530

    Document header added

--- studentdetails.py
+++ studentdetails.py
@@ -1,7 +1,13 @@
+""""
+#Author: Abhirami
+Date:01-10-2024
+python program is to get the student details
+version:1.0
+"""
 name=input("enter the student name:")
 roll_number=input("enter the roll number:")
 cgpa=float(input('enter the cgpa:'))
 percentage=cgpa*10
 print('Name of the student:', name)
 print('roll number:',roll_number)
-print('total percentage=', pecentage)
+print('total percentage=',percentage)
