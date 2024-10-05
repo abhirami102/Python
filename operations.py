b18cbe553d840df68b15ec56b3b375e6dab7d0d5
abhirami102 <137210545+abhirami102@users.noreply.github.com>
2024-10-05 15:58:50 +0530

    Add files via upload

---
+++ operations.py
@@ -0,0 +1,15 @@
+"""
+author:abhirami
+date:05-10-24
+to perform various operations on integers
+"""
+number1=10
+number2=5
+
+sum=number1+number2
+division=number1/number2
+print("sum:",sum,"," "division:",division)
+print("Is number1 greater than number2?",number1>number2)
+print("Are number1 and number2 equal?",number1==number2)
+print("Logical AND:",number1>number2 and number2<number1)
+print("Logical OR:", number1>number2 or number2>number1)
