2173ba1ea115ba2d9f2757fadb1b8d1db3aba280
abhirami102 <137210545+abhirami102@users.noreply.github.com>
2024-10-05 15:59:04 +0530

    Add files via upload

---
+++ twinkle twinkle.py
@@ -0,0 +1,8 @@
+""""
+author:abhirami
+date:05-10-24"
+to ptint string twinkle twinkle little star
+"""
+str="Twinkle, twinkle,little star,\n\t\tHow I wonder what you are!\n\t\t\t\tUp above the world so high,\n\t\t\t\tLike a diamond in the sky.\nTwinkle, twinkle, little star,\n\t\tHow I wonder what you are"
+
+print(str)
