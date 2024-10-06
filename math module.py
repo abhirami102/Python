ecaec949c938afd0d8911d0844f6ae737b57fb73
Abhirami <137210545+abhirami102@users.noreply.github.com>
2024-10-06 17:07:12 +0530

    math module.py

--- math module.py
+++ math module.py
@@ -3,8 +3,9 @@ author:abhirami
 date:05-10-24
 to use functions from math module to perfom some operations"""
 
-num=int(input("Enter a number:"))
+
 import math
+num=int(input("Enter a number:"))
 print("Square root of",num,":",math.sqrt(num))
 print("Factorial of",num,":",math.factorial(num))
 print(num,"raised to power 2:",math.pow(num,2))
