d468ebfcad6cf58d2e339204a1c72f973f803ada
Abhirami <137210545+abhirami102@users.noreply.github.com>
2024-10-08 15:05:47 +0530

    Add files via upload

---
+++ basic_arithmetic_operations.py
@@ -0,0 +1,23 @@
+""""
+
+author:Abhirami B
+Date: 08-10-24
+to do some bsic arithmatic operations
+
+"""
+num1=int(input("Enter the first number:"))
+num2=int(input("Enter the second number:"))
+num3=int(input("Enter a third number:"))
+
+add=num1+num2
+print("The sum of", num1, "and",num2,"is:",add)
+subtract=num2-num1
+print("The difference when",num2,"is sutracted from",num1,"is:",subtract)
+multiply=num1*num2
+print("The product of",num1,"and",num2,"is:",multiply)
+divide=num1/num2
+print("The quotient when",num1,"is divided by",num2,"is:",divide)
+modulus=num1%num2
+print("The remainder when",num1,"is divided by",num2,"is:",modulus)
+combine=(num1 + num2) * num3 / 2
+print("The result of(" ,num1,"+" , num2, ")*", num3,"/2" ,"is:",combine)
