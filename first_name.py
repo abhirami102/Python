7abd3f0b907f350bded97d28d81d1a5dfffb7db7
Abhirami <137210545+abhirami102@users.noreply.github.com>
2024-10-08 15:06:19 +0530

    Add files via upload

---
+++ first_name.py
@@ -0,0 +1,11 @@
+""""
+author:Abhirami B
+date:08-10-24
+"""
+
+first_name=input("enter your first name:")
+last_name=input("enter your last name:")
+full_name=first_name+" "+last_name
+print(full_name)
+length=len(first_name)
+print("first_name=",full_name[:length])
