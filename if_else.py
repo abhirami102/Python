ae4ef66d61fdd903941b483fe92bf51dfab779b5
Abhirami <137210545+abhirami102@users.noreply.github.com>
2024-10-15 14:21:08 +0530

    Add files via upload

---
+++ if_else.py
@@ -0,0 +1,12 @@
+"""
+author:abhirami
+date:15-10-24
+to determine the large of two numbers
+"""
+
+number1=int(input("Enter the 1st number:"))
+number2=int(input("Enter the 2nd number"))
+if number1>number2:
+    print(number1,"is the largest number")
+else:
+    print(number2,"is the largest number")
