c1d08d3c15ec527683a707687a0ec454aa70ec84
Abhirami <137210545+abhirami102@users.noreply.github.com>
2024-10-15 16:17:57 +0530

    Add files via upload

---
+++ sum_of_digit.py
@@ -0,0 +1,12 @@
+"""
+author:abhirami b
+date:15-15-24\
+to find the sum of digit of a number
+"""
+number=int(input("Enter the number:"))
+sum=0
+while number>0:
+    r=number%10
+    number=number//10
+    sum=sum+r
+print(sum)
