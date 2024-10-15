f99b73a80fa806b88b39e8cc8d24d32360373a97
Abhirami <137210545+abhirami102@users.noreply.github.com>
2024-10-15 16:06:57 +0530

    Add files via upload

---
+++ upto_10.py
@@ -0,0 +1,10 @@
+"""
+author:abhirami b
+date:15-15-24\
+to print odd numbers upto n
+"""
+limit=int(input("Enter your limit:"))
+odd_number=1
+while (odd_number<=limit):
+    print(odd_number,"\t",end="")
+    odd_number+=2
