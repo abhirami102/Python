8fb989796e01304612ceb59e28e2e5fd2704f2d2
Abhirami <137210545+abhirami102@users.noreply.github.com>
2024-10-15 14:47:41 +0530

    Add files via upload

---
+++ entry_ticket.py
@@ -0,0 +1,12 @@
+""""
+author:abhirami b
+date:15-10-24
+to check the age for zoo entry"""
+
+age=int(input("Enter your age:"))
+if age<10:
+    print("The fare=7")
+elif age>=10 and age<60:
+    print("The fare=10")
+else:
+    print("The fare=5")
