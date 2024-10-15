24d97a4625ad7405af7dea4791a5c10a516065fd
Abhirami <137210545+abhirami102@users.noreply.github.com>
2024-10-15 15:00:08 +0530

    Add files via upload

---
+++ drivers_license.py
@@ -0,0 +1,10 @@
+"""
+author:abhirami
+date:15-1024
+to check the eligiblity of driving license
+"""
+age=int(input("Enter your age:"))
+if age>=16:
+    print("Eligible for driving license")
+else:
+    print("Not eligible for driving license")
