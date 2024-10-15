6acd106cefd5591ea77f89c40d65e3f19616ae2e
Abhirami <137210545+abhirami102@users.noreply.github.com>
2024-10-15 15:03:57 +0530

    Add files via upload

---
+++ division_by_5.py
@@ -0,0 +1,11 @@
+"""
+author:abhirami
+date:15-10-24
+tp check whether a number is divisible by 5
+"""
+
+number=int(input("Enter a number:"))
+if (number%5==0):
+    print("Divisible by 5")
+else:
+    print("Not divisible by 5")
