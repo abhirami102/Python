5eba5206bce7ca6583dc0522155e8e256174d19d
Abhirami <137210545+abhirami102@users.noreply.github.com>
2024-10-15 14:49:29 +0530

    Add files via upload

---
+++ smallest_num.py
@@ -0,0 +1,16 @@
+""""
+author:abhirami b
+date:15-10-24
+to determine the smallest of three numbers
+"""
+
+number1=int(input("Enter the first number:"))
+number2=int(input("Enter the second number:"))
+number3=int(input("Enter the third number:"))
+
+if number1<number3 and number1<number3:
+    print(number1,"is the smallest number")
+elif number2<number3:
+    print(number2,"is the samllest")
+else:
+    print(number3,"is the smallest")
